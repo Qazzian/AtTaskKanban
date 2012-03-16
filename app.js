ea474415e3f071c984916ad1e59b3ef554ac0f9a
Ian Wallis <ian.wallis@grapplemobile.com>
2012-03-16 08:23:37 +0000

    Example of using route parameters. Also small changes to the style and template.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.configure('production', function(){
 // Routes
 
 //app.get('/styles/*', routes.styles);
-app.get('/', routes.index);
+app.get('/:page?', routes.index);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
