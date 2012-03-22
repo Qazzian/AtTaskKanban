4558d8730b93369ce673606549b9f361924cb457
Ian Wallis <ian.wallis@grapplemobile.com>
2012-03-22 18:28:03 +0000

    Moved layouts into a seperate directory

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.configure('production', function(){
 //app.get('/styles/*', routes.styles);
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var sio = socketio.listen(app);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.index = function(req, res){
 	console.log('Get index.html');
 	try {
-		res.render('index', {title: 'Express Template', page: req.params.page, layout: 'layout'});
+		res.render('index', {title: 'Express Template', page: req.params.page, layout: 'layouts/layout'});
 	}
 	catch (e) {
 		console.log('ERROR: ' + e);
